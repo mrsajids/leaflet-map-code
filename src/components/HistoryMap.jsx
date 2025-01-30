8d314d1afeeac83f2f4eff1a57ae6d3786a0aaa7
Sajeed <syedsajeed6969@gmail.com>
2025-01-31 01:05:22 +0530

    changes

--- src/components/HistoryMap.jsx
+++ src/components/HistoryMap.jsx
@@ -19,64 +19,55 @@ import redarrow from './redarrow.png';
 const HistoryMap = ({ }) => {
   const featureRef = useRef();
   const map = useMap({});
-  const [vehicleMarkers, setVehicleMarkers] = useState([]);
+  const [routeArrowMarkers, setRouteArrowMarkers] = useState([]);
   const [startFlagPositions, setStartFlagPositions] = useState([]);
   const [endFlagPosition, setEndFlagPosition] = useState([]);
-  const [vehicleHistory, setVehicleHistory] = useState([]);
   const [segments, setSegments] = useState([]);
 
 
   //   let vehicleHistory = [];
   useEffect(() => {
-
     fetchHistory();
-    // const bounds = featureRef.current.getBounds();
-    // const tempArr = [
-    //   [28.7041, 77.1025], // Delhi
-    //   [28.5355, 77.391], // Noida
-    //   [28.4595, 77.0266], // Gurgaon
-    //   [28.6139, 77.209], // Central Delhi];
-    // ];
-
-    // if (featureRef.current) {
-    //   // const bounds = featureRef.current.getBounds();
-    //   map.flyToBounds([tempArr], 10);
-    //   setTimeout(() => {
-    //     setVehicleHistory(tempArr);
-    //   }, 2000);
-    // }
   }, []);
 
   const fetchHistory = async () => {
-    // fetch
+    // fetch code
+    // ....
     const vehicleData = historyData;
-    // const totalDistance = res?.entity?.totaldistance;
-    // setTotalDistance(totalDistance);
-
+    // first flag
     const initialLat = Number(vehicleData[0].latitude);
     const initialLng = Number(vehicleData[0].longitude);
-
-    // setVehicleInfo(vehicleData[0]);
-
+    // last flag
     const lastLat = Number(vehicleData[vehicleData.length - 1].latitude);
     const lastLng = Number(vehicleData[vehicleData.length - 1].longitude);
-    // setTimeout(() => {
-    // const lastLat = Number(vehicleData.slice(-1)[0].latitude);
-    // const lastLng = Number(vehicleData.slice(-1)[0].longitude);
 
     setStartFlagPositions([initialLat, initialLng]);
-    console.log(lastLat);
     setEndFlagPosition([lastLat, lastLng]);
-    // vehicleData.map((item) => {
-    //   const lat = Number(item.latitude);
-    //   const lng = Number(item.longitude);
-    //   const newPos = [lat, lng];
-    //   tempPath.push(newPos);
 
+    // drawing new Route
+    const newRoute = drawRoute(vehicleData);
+
+    setTimeout(() => {
+      setRouteArrowMarkers(vehicleData);
+      setSegments(newRoute);
+    }, 2500);
+
+    const routeBounds = newRoute.flatMap(item => item.positions);
+
+    // console.log(aaa);
+    map.flyToBounds([routeBounds], map.getZoom(), {
+      animate: true, // Optional: smooth transition
+      duration: 3, // Optional: specify the duration of the flyTo
+    });
+
+  }
+
+  const drawRoute = (vehicles) => {
+    // line color based on Inside or Outside
     const updatedSegments = [
       { positions: [], color: "#3f3c3c" }, // Default to blue for "Inside"
     ];
-    vehicleData.forEach((item) => {
+    vehicles.forEach((item) => {
       const lat = Number(item.latitude);
       const lng = Number(item.longitude);
       const newPos = [lat, lng];
@@ -95,40 +86,12 @@ const HistoryMap = ({ }) => {
       }
     });
 
-    setTimeout(() => {
-      setVehicleMarkers(vehicleData);
-      setSegments(updatedSegments); // });
-      // setPath(tempPath);
-    }, 2500);
-    // setIsHistoryPlotted(true);
-    // }, 2500);
-    // console.log(tempPath);
-
-    // if (tempPath) {
-    //   // Automatically adjusts zoom level to fit all markers
-    //   map.fitBounds(tempPath, {
-    //     padding: [50, 50],
-    //     maxZoom: 15,
-    //     minZoom: 5,
-    //     animate: true,
-    //   });
-    // }
-    // if (initialLat && initialLng)
-    // want to spread item.positions
-
-    const aaa = updatedSegments.flatMap(item => item.positions);
-
-
-    // console.log(aaa);
-    map.flyToBounds([aaa], map.getZoom(), {
-      animate: true, // Optional: smooth transition
-      duration: 3, // Optional: specify the duration of the flyTo
-    });
-
+    return updatedSegments;
   }
 
 
-  const customIcon = (vehicle) => {
+
+  const getArrowIcon = (vehicle) => {
     // change color of vehicle accroing to its status
     const vehicleSpeed = Number(vehicle.vehiclespeed);
 
@@ -147,9 +110,9 @@ const HistoryMap = ({ }) => {
     // change color of vehicle accroing to its status
     return new L.DivIcon({
       className: "", // Custom class for the icon
-      html: `<img class="flag-icon" height="35" src=${src} alt="" />`,
-      iconSize: [0, 0], // Size of the whole icon container (including text)
-      iconAnchor: [13, 13], // Anchor the icon to its center
+      html: `<img class="flag-icon" height="40" src=${src} alt="" />`,
+      // iconSize: [0, 0], // Size of the whole icon container (including text)
+      iconAnchor: [0, 0], // Anchor the icon to its center
     });
   };
 
@@ -165,12 +128,12 @@ const HistoryMap = ({ }) => {
       )}
 
       {/* arrows showing history */}
-      {vehicleMarkers.length > 0 &&
-        vehicleMarkers.map((vehicle, index) => (
+      {routeArrowMarkers.length > 0 &&
+        routeArrowMarkers.map((vehicle, index) => (
           <Marker
             key={index}
             position={[Number(vehicle.latitude), Number(vehicle.longitude)]}
-            icon={customIcon(vehicle)}
+            icon={getArrowIcon(vehicle)}
             zIndexOffset={150}
             eventHandlers={{
               // click: () => setVehicleInfo(vehicle),
